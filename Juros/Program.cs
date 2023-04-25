8391b92529d8cc70d667e1219788948130f64a41
Danilo Correia <100938834+ProjetDaniloCorreia1c@users.noreply.github.com>
2023-04-25 10:43:15 -0300

    Add files via upload

---
+++ Juros/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Juros;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Conta contaum = new Conta();
+        //dando valor
+        contaum.Deposito(2000);
+
+        contaum.DefinirLimite(500);
+        
+        contaum.AlterarNumero(12345);
+
+        contaum.Sacar(1500);
+
+        Console.WriteLine("Valor é: "+contaum.ConsultaSaldo());
+
+        contaum.CalculaJuros(1000,12,6);
+    }
+}
